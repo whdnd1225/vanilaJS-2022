356fafd38a9d6e2fafe559031fd04b1dbc6a4e6d
jiwoong.jo <whdnd1225@naver.com>
2022-11-21 17:14:00 +0900

    정리

--- js/background.js
+++ js/background.js
@@ -1,13 +1,4 @@
-const images = [
-  '0.jpeg',
-  '1.jpeg',
-  '2.jpeg',
-  '3.jpeg',
-  '4.jpeg',
-  '5.jpeg',
-  '6.jpeg',
-  '7.jpeg',
-];
+const images = ['0.jpg', '1.jpg'];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,17 @@
-const clock = document.querySelector('h2#clock');
+const clockDay = document.querySelector('h2#clock-day');
+const clockTime = document.querySelector('h2#clock-time');
 
 function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
   const seconds = String(date.getSeconds()).padStart(2, '0');
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  const day = date.toDateString().substring(0, 3);
+  const month = date.toDateString().substring(4, 7);
+
+  clockDay.innerText = `${day} ${month}`;
+  clockTime.innerText = `${hours}:${minutes}:${seconds}`;
+  console.log(date.toDateString());
 }
 
 getClock();
